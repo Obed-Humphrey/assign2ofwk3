c0bf4800aaacb70d36f32ee648fda89c8c8e72a8
Obed-Humphrey <133528651+Obed-Humphrey@users.noreply.github.com>
2023-05-24 22:33:47 +0100

    Create assign2wk3.php

---
+++ assign2wk3.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "Obed Chinecherem";
+$regnumber = "2021/HND/33788/CS";
+$SOO = "Abia State";
+$topic_of_project = "Design and Implementation of an AI powered chat bot on medical enquiry";
+
+// Output the information to the screen.
+echo "Name: $name<br>";
+echo "Regnumber: $regnumber<br>";
+echo "State of origin: $SOO<br>";
+echo "Project topic: $topic_of_project<br>";
+
+?>
